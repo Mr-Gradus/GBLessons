ef5fbe9e03c56ef969b3ddaf54b71663952d5f1c
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-04 14:20:40 +0500

    Update main.cpp
    practice 2 task 1 done

--- main.cpp
+++ main.cpp
@@ -5,24 +5,48 @@
 using namespace std;
 //=================Task_1=====================
 
-class Power 
+class Person 
+{
+protected:
+	string m_name;
+	int m_age;
+	string m_gender;
+	int m_weight;
+public:
+	Person(string name, int age, string gender, int weight) : m_name(name), m_age(age), m_gender(gender), m_weight(weight)
+	{	}
+
+	~Person() {}
+};
+
+class Student : private Person
 {
 private:
-	double num1;
-	double num2;
-
+	int m_years;
+	static int count;
 public:
-	void setPower()
+	static int getCount()
 	{
-		num1 = 4.13;
-		num2 = 2.22;
-	}
-	void Calculate()
-	{
-		cout << num1 << " в степени " << num2 << " = " << pow(num1, num2) << endl;
+		return count;
 	}
 	
+	Student(string name, int age, string gender, int weight, int years) : m_years(years), Person(name, age, gender, weight) {
+			count++;
+	}
+		
+	
+	//~Student() {}
+
+	void print() const
+	{
+		cout << "Имя студента: " << m_name << ".\n" << "Возраст: " << \
+			m_age << ".\n" << "Пол: " << m_gender << ".\n" << \
+			"Вес: " << m_weight << ".\n" << "Курс: " << m_years << ".\n" << endl;
+	}
 };
+
+int Student::count = 0;
+
 
 //=================Task_2=====================
 
@@ -59,8 +83,16 @@ int main()
 	setlocale(0, "rus");
 
 	//=================Task_1=====================
-	cout << "===Task 1===" << endl;
+	cout << "==========Task 1==========" << endl;
 
+	Student student("Иван",21,"муж",55,4);
+	student.print();
+	Student student2("Михаил", 19, "муж", 50, 2);
+	student2.print();
+	Student student3("Алиса", 18, "жен", 47, 1);
+	student3.print();
+	cout << "Количество студентов: " <<Student::getCount() << endl;
+	
 	
 	//=================Task_2=====================
 	
