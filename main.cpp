cafec3b2f46ddaaf28d51aac4e67da3da7005baa
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-04 12:57:34 +0500

    Start practice 2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 ﻿#include <iostream>
 #include <locale>
-#include <cmath>
-//#include <cstdint>
-#include <cassert>
+#include <string>
 
 using namespace std;
 //=================Task_1=====================
@@ -49,51 +47,9 @@ public:
 
 	}
 
-	void print() // Переделать в отдельную функцию??
-	{
-		cout << "Red." << m_red << endl << "Green." << m_green << endl << "Blue." << m_blue << endl << "Alpha." << m_alpha << endl;
-	}
 };
 
 //=================Task_3=====================
-
-class Stack
-{
-private:
-	int m_array[10];
-	int m_length;
-public:
-	void reset()
-	{
-		m_length = 0;
-		for (int i = 0; i < 10; i++)
-			m_array[i] = 0;
-	}
-	bool push(int cell)
-	{
-		if (m_length == 10)
-			return false;
-
-		else (m_array[m_length++] = cell);
-			return true;
-	}
-
-	int pop()
-	{
-		assert (m_length > 0);
-
-		return m_array[--m_length];
-	}
-
-	void print()
-	{
-		cout << "( ";
-		for (int i = 0; i < m_length; i++)
-			cout << m_array[i] << ' ';
-		cout << ")" << endl;
-	}
-	
-};
 
 
 
@@ -105,35 +61,15 @@ int main()
 	//=================Task_1=====================
 	cout << "===Task 1===" << endl;
 
-	Power sum;
-	sum.setPower();
-	sum.Calculate();
 	
 	//=================Task_2=====================
 	
 	cout << endl << "===Task 2===" << endl;
 
-	RGBA colors(3, 4, 5, 6);
-	colors.print();
 	
 	//=================Task_3=====================
 	cout << endl << "===Task 3===" << endl;
 
-	Stack stack;
-	stack.reset();
-	stack.print();
-	
-	stack.push(3);
-	stack.push(7);
-	stack.push(5);
-	stack.print();
-	
-	stack.pop();
-	stack.print();
-
-	stack.pop();
-	stack.pop();
-	stack.print();
 
 	return 0;
 }
