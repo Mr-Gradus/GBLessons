22cf1ddc4ea021b8c14d7aafe679a9a18ef527ef
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-08 16:32:27 +0500

    Practice 3
    Task 1,2  done
    task 3 in progress

--- main.cpp
+++ main.cpp
@@ -5,225 +5,192 @@
 using namespace std;
 
 //=================Task_1=====================
-	class Person
-	{
-	protected:
-		string m_name;
-		int m_age;
-		string m_gender;
-		int m_weight;
-	public:
-		Person(string name, int age, string gender, int weight) : m_name(name), m_age(age), m_gender(gender), m_weight(weight)
-		{	}
 
-		~Person() {}
+	class Figure 	{
+	public:
+	Figure () {}
+	virtual ~Figure() {};
+	virtual void area() const = 0;
+
 	};
 
-	class Student : private Person
-	{
-	private:
-		int m_years;
-		static int count;
+	class Parallelogram : public Figure {
+	//protected:
+
 	public:
-		static int getCount()
-		{
-			return count;
-		}
-
-		Student(string name, int age, string gender, int weight, int years) : m_years(years), Person(name, age, gender, weight) {
-			count++;
-		}
-
-
-		~Student() {}
-
-		void print() const
-		{
-			cout << "Имя студента: " << m_name << ".\n" << "Возраст: " << \
-				m_age << ".\n" << "Пол: " << m_gender << ".\n" << \
-				"Вес: " << m_weight << ".\n" << "Курс: " << m_years << ".\n" << endl;
+		Parallelogram () {}
+		void area() const override {
+			cout << "Параллелограм S = a * h" << endl;
 		}
 	};
 
-	int Student::count = 0;
+	class Circle : public Figure {
+	//private:
+
+	public:
+		Circle () {}
+		void area() const override {
+			cout << "Круг S = piR^" << endl;
+		}
+	};
+
+	class Rectangle : private Parallelogram {
+	//private:
+
+	public:
+		Rectangle () {}
+		void area() const override {
+			cout << "Прямоугольник S = a * b" << endl;
+		}
+	};
+
+	class Square : private Parallelogram {
+	//private:
+
+	public:
+		Square () {}
+		void area() const override {
+			cout << "Квадрат S = a^" << endl;
+		}
+	};
+
+	class Rhombus : private Parallelogram {
+	//private:
+
+	public:
+		Rhombus () {}
+		void area() const override {
+			cout << "Ромб S = a^ * sin A" << endl;
+		}
+	};
 
 //=================Task_2=====================
 
-class Fruit {
-protected:
-	string name;
-	string color;
-public:
-	Fruit() : name("fruit"), color("color") {}
-	
-	~Fruit() {}
-};
-class Apple : private Fruit {
-	string a_color;
-public:
-	Apple(string c) : a_color(c) {}
-	Apple() {}
-
-	~Apple() {}
-	
-	string getName() const
-	{ 
-		return "apple";
-	}
-	string getColor() const 
-	{ 
-		return a_color;
-	}
-};
-	
-	
-class Banana : private Fruit {
-public:
-	Banana() {} 
-	~Banana() {}
-	string getName() const 
-	{ 
-		return "banana";
-	}
-	string getColor() const 
-	{ 
-		return "yellow";
-	}
-};
+	class Car {
+	protected:
+		string company;
+		string model;
+	public:
+		Car() {}
 		
-class GrannySmith : private Apple {
+		/*Car(string company, string model) {
+			cout << company << "+++" << model << endl;
+		}*/
+	};
 
-public:
-	GrannySmith() { }
-	~GrannySmith() { }
-	string getName() const
-	{
-		return "Granny Smith";
-	}
-	string getColor() const
-	{
-		return "green";
-	}
-};
+	class PassengerCar : virtual public Car {
+	public:
+		PassengerCar(string c, string m) : Car() {
+			cout << c << "*" << m << endl;
+		}
+	};
 
+	class Bus : virtual public Car {
+	public:
+		Bus(string c, string m) : Car() {
+			cout << c << "=" << m << endl;
+		}
+	};
+
+	class Minivan : public PassengerCar, public Bus {
+	public:
+		Minivan(string c, string m) : PassengerCar(c,m), Bus(c,m) {
+			cout << c << "-" << m << endl;
+		}
+	};
 
 //=================Task_3=====================
 
+	class Fraction {
+	public:
+		double numenator;
+		double denomenator;
+	public:
+		Fraction(double num, double den) : numenator(num), denomenator(den) {
+			if (den == 0)
+				cout << "Ошибка - знаменатель не может быть 0!!!" << endl;
+			
+		}
+		
+		int getNum() const {
+		return numenator;
+		}
+		int getDen() const {
+		return denomenator;
+		}
+		
+		
+		friend Fraction operator+ (const Fraction& n1, const Fraction& n2);
+		friend Fraction operator- (const Fraction& n1, const Fraction& n2);
+		friend Fraction operator* (const Fraction& n1, const Fraction& n2);
+		friend Fraction operator/ (const Fraction& n1, const Fraction& n2);
+	};
+
+	Fraction operator+ (Fraction& n1, Fraction& n2)	{
+		return Fraction(n1.numenator * n2.denomenator + n2.numenator * n1.denomenator, n1.denomenator * n2.denomenator);
+	}
+
+	Fraction operator- (Fraction& n1, Fraction& n2) {
+		return Fraction(n1.numenator * n2.denomenator + n2.numenator * n1.denomenator, n1.denomenator * n2.denomenator);
+	}
+	/*
+	class Fraction {
+	private:
+		int number;
+	public:
+		Fraction(int num) : number(num) {}
+		int getNum() const {
+			return number;
+		}
+		friend Fraction operator+ (const Fraction& n1, const Fraction& n2);
+	};
+
+	Fraction operator+ (const Fraction& n1, const Fraction& n2) {
+		return Fraction(n1.number + n2.number);
+	}
+	*/
+
+
 int main()
 {	
-	setlocale(0, "rus");
+	{
+		setlocale(0, "rus");
 
-	//=================Task_1=================
-	
+		//=================Task_1=================
+
 		cout << "==========Task 1==========" << ".\n" << endl;
 
-		Student student("Иван", 21, "муж", 55, 4);
-		student.print();
-		Student student2("Михаил", 19, "муж", 50, 2);
-		student2.print();
-		Student student3("Алиса", 18, "жен", 47, 1);
-		student3.print();
-		cout << "Количество студентов: " << Student::getCount() << endl;
-	
-	
-	//=================Task_2=====================
-	
-	cout << endl << "==========Task 2==========" << ".\n" << endl;
+		Parallelogram parallelogram;
+		parallelogram.area();
 
-	Apple a("red");
-	Banana b;
-	GrannySmith c;
+		Circle circle;
+		circle.area();
 
-	cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
-	cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
-	cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
-	
-	
-	
-	
+		Rectangle rectangle;
+		rectangle.area();
+
+		Square square;
+		square.area();
+
+		Rhombus rhombus;
+		rhombus.area();
+
+		//=================Task_2=====================
+
+		cout << endl << "==========Task 2==========" << ".\n" << endl;
+
+
+		PassengerCar sedan("Audi", "A4");
+		Bus bus("МАЗ", "105");
+		Minivan car("VW", "Multivan"); //обращается к обоим базовым классам, а если сделать конструктор с данными к классу Car то и к нему 
+
+	}
 	//=================Task_3=====================
 	cout << endl << "==========Task 3==========" << ".\n" << endl;
 
- 
-	/*
-	class Game { // класс с графической реализацие экрана игры
-	private:
-	
-	
-	public:
-
-	};
-	
-	class Animation : private Game { // не уверен но возможно анимацию карт в отдельный класс надо 
-	private:
-
-
-	public:
-
-	};
-	class Players {  // класс игроки
-	private:
-
-
-	public:
-
-	};
-	
-	class Dealer { //класс раздающий, AI
-	private:
-
-
-	public:
-
-	};
-	
-	class Table {  // класс стол информация про карты на столе, карты на руках у игроков - возможно это надо в отдельный класс
-	private:
-
-
-	public:
-
-	};
-	
-	class Deck  : private Table {  // класс колода
-	private:
-		string name; //название колоды если необходимо
-		int cards; //количество карт в колоде
-	public:
-	};
-	
-	class Rule { // правила игры
-	private:
-
-
-	public:
-
-	};
-
-	class Bet  : private Rule {  // класс содержащий инфу про ставки/деньги/очки
-	private:
-
-
-	public:
-
-	};
-
-	class Check  : private Rule{ // проверка количества очков, выигрыш проигрыш
-	private:
-
-
-	public:
-	};
-	
-	class Stat  : private Check {  // статистика игрока
-	private:
-
-
-	public:
-
-	};
-	*/
-	
+	Fraction n1(1,5), n2(3,4);
+	Fraction res = n1 + n2;
+	cout << res.getNum() << "/" << res.getDen() << endl;
 	return 0;
 }
 
